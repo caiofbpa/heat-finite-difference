08716e35c88e0af2519d96824dad9a5b53a64848
Caio Fernando Bertoldi Paes de Andrade <caiofbpa@gmail.com>
2013-07-30 23:24:34 +0100

    Time iteration and refactorings

--- test/test.cc
+++ test/test.cc
@@ -1,90 +1,122 @@
 #include <gtest/gtest.h>
 #include <vector>
 
-class HeatFiniteDifferenceTestSuite : public testing::Test {
-protected:
+using namespace std;
 
-	double delta = .001;
+double delta = .001;
 
-};
-
-std::vector<double> calculateNextTemperatures(std::vector<double> temperatures){
+double simulateNextTemperature(double leftNeighbour, double previousTemperature, double rightNeighbour){
 	double fourierNumber = 0.2;
-	std::vector<double> nextTemperatures(temperatures);
+	double nextTemperature = previousTemperature + fourierNumber * (rightNeighbour - (2 * previousTemperature) + leftNeighbour);
+	return nextTemperature;
+}
 
-	for(int i = 1; i < (temperatures.size()-1); i++){
-		double leftNeighbour = temperatures[i - 1];
-		double previousTemperature = temperatures[i];
-		double rightNeighbour = temperatures[i + 1];
-		nextTemperatures[i] = previousTemperature + fourierNumber * (rightNeighbour - (2 * previousTemperature) + leftNeighbour);
-	}
-	
+vector<double> simulateNextTemperatures(vector<double> temperatures){
+	vector<double> nextTemperatures(temperatures);
+	for(int i = 1; i < (temperatures.size() - 1); i++)
+		nextTemperatures[i] = simulateNextTemperature(temperatures[i - 1], temperatures[i], temperatures[i + 1]);
 	return nextTemperatures;
 }
 
-TEST_F(HeatFiniteDifferenceTestSuite, ZeroTemperatures_MiddleShouldStayAtZero){
-	std::vector<double> temperatures(3);
-	temperatures[0] = 0.0;
-	temperatures[1] = 0.0;
-	temperatures[2] = 0.0;
-	std::vector<double> nextTemperatures = calculateNextTemperatures(temperatures);
-	ASSERT_NEAR(0, nextTemperatures[1], delta);
+vector<double> simulateTemperaturesAfterTime(vector<double> temperatures, int seconds){
+	for(int i = 0; i < seconds; i++)
+		temperatures = simulateNextTemperatures(temperatures);
+	return temperatures;
 }
 
-TEST_F(HeatFiniteDifferenceTestSuite, EqualTemperatures_MiddleShouldStayAtSameTemperature){
-	std::vector<double> temperatures(3);
-	temperatures[0] = 1.0;
-	temperatures[1] = 1.0;
-	temperatures[2] = 1.0;
-	std::vector<double> nextTemperatures = calculateNextTemperatures(temperatures);
-	ASSERT_NEAR(1, nextTemperatures[1], delta);
+void assertVectorsHaveEqualValues(vector<double> expected, vector<double> actual){
+	for(int i = 0; i < expected.size(); i++)
+		ASSERT_NEAR(expected[i], actual[i], delta);
 }
 
-TEST_F(HeatFiniteDifferenceTestSuite, GivenACoolNeighbour_MiddleTemperatureShouldCool){
-	std::vector<double> temperatures(3);
-	temperatures[0] = 20.0;
-	temperatures[1] = 20.0;
-	temperatures[2] = 0.0;
-	std::vector<double> nextTemperatures = calculateNextTemperatures(temperatures);
-	ASSERT_NEAR(16, nextTemperatures[1], delta);
+TEST(HeatFiniteDifferenceTestSuite, ZeroTemperatures_MiddleShouldStayAtZero){
+	double initial[] = {0.0, 0.0, 0.0};
+	double expected[] = {0.0, 0.0, 0.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
 }
 
-TEST_F(HeatFiniteDifferenceTestSuite, GivenAHotNeighbour_MiddleTemperatureShouldHeat){
-	std::vector<double> temperatures(3);
-	temperatures[0] = 20.0;
-	temperatures[1] = 20.0;
-	temperatures[2] = 50.0;
-	std::vector<double> nextTemperatures = calculateNextTemperatures(temperatures);
-	ASSERT_NEAR(26, nextTemperatures[1], delta);
+TEST(HeatFiniteDifferenceTestSuite, EqualTemperatures_MiddleShouldStayAtSameTemperature){
+	double initial[] = {1.0, 1.0, 1.0};
+	double expected[] = {1.0, 1.0, 1.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
 }
 
-TEST_F(HeatFiniteDifferenceTestSuite, ThreeTemperatures_Integration_Case1){
-	std::vector<double> temperatures(3);
-	temperatures[0] = 24.802;
-	temperatures[1] = 29.657;
-	temperatures[2] = 34.604;
-	std::vector<double> nextTemperatures = calculateNextTemperatures(temperatures);
-	ASSERT_NEAR(29.676, nextTemperatures[1], delta);
+TEST(HeatFiniteDifferenceTestSuite, GivenACoolNeighbour_MiddleTemperatureShouldCool){
+	double initial[] = {20.0, 20.0, 0.0};
+	double expected[] = {20.0, 16.0, 0.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
 }
 
-TEST_F(HeatFiniteDifferenceTestSuite, ThreeTemperatures_Integration_Case2){
-	std::vector<double> temperatures(3);
-	temperatures[0] = 39.976;
-	temperatures[1] = 44.986;
-	temperatures[2] = 50.0;
-	std::vector<double> nextTemperatures = calculateNextTemperatures(temperatures);
-	ASSERT_NEAR(44.987, nextTemperatures[1], delta);
+TEST(HeatFiniteDifferenceTestSuite, GivenAHotNeighbour_MiddleTemperatureShouldHeat){
+	double initial[] = {20.0, 20.0, 50.0};
+	double expected[] = {20.0, 26.0, 50.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
 }
 
-TEST_F(HeatFiniteDifferenceTestSuite, FourTemperatures_Integration_Case1){
-	std::vector<double> temperatures(4);
-	temperatures[0] = 20.0;
-	temperatures[1] = 20.0;
-	temperatures[2] = 16.0;
-	temperatures[3] = 0.0;
-	std::vector<double> nextTemperatures = calculateNextTemperatures(temperatures);
-	ASSERT_NEAR(19.2, nextTemperatures[1], delta);
-	ASSERT_NEAR(13.6, nextTemperatures[2], delta);
+TEST(HeatFiniteDifferenceTestSuite, ThreeTemperatures_Integration_Case1){
+	double initial[] = {24.802, 29.657, 34.604};
+	double expected[] = {24.802, 29.676, 34.604};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
+}
+
+TEST(HeatFiniteDifferenceTestSuite, ThreeTemperatures_Integration_Case2){
+	double initial[] = {39.976, 44.986, 50.0};
+	double expected[] = {39.976, 44.987, 50.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
+}
+
+TEST(HeatFiniteDifferenceTestSuite, FourTemperatures_Integration_Case1){
+	double initial[] = {20.0, 20.0, 16.0, 0.0};
+	double expected[] = {20.0, 19.2, 13.6, 0.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
+}
+
+TEST(HeatFiniteDifferenceTestSuite, FourTemperatures_Integration_Case2){
+	double initial[] = {38.4, 30.0, 22.0, 26.0};
+	double expected[] = {38.4, 30.08, 24.4, 26.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
+}
+
+TEST(HeatFiniteDifferenceTestSuite, SevenTemperatures_Integration){
+	double initial[] = {20.0, 50.0, 40.0, 30.0, 20.0, 10.0, 50.0};
+	double expected[] = {20.0, 42.0, 40.0, 30.0, 20.0, 20.0, 50.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 1);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
+}
+
+TEST(HeatFiniteDifferenceTestSuite, SevenTemperatures_Integration_AfterLongTime){
+	double initial[] = {20.0, 50.0, 40.0, 30.0, 20.0, 10.0, 50.0};
+	double expected[] = {20.0, 24.987, 29.977, 34.973, 39.977, 44.987, 50.0};
+	vector<double> temperatures (initial, initial + sizeof(initial) / sizeof(initial[0]));
+	vector<double> expectedTemperatures (expected, expected + sizeof(expected) / sizeof(expected[0]));
+	vector<double> nextTemperatures = simulateTemperaturesAfterTime(temperatures, 99);
+	assertVectorsHaveEqualValues(expectedTemperatures, nextTemperatures);
 }
 
 int main(int argc, char** argv) {
